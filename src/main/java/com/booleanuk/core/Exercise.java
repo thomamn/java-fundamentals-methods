8d20bf622cc78e6635f2d62b8d296399ecf81704
TMajlu <thomasmajnie@gmail.com>
2024-08-12 15:17:49 +0200

    Completed this exercise

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -35,7 +35,7 @@ public class Exercise extends ExerciseBase {
         Complete this method so that it increases the number given by 1 and returns the result
      */
     public int increment(int number) {
-        return 0;
+        return number+1;
     }
 
     /*
@@ -48,8 +48,8 @@ public class Exercise extends ExerciseBase {
         Nathan | Hi, Nathan :)
         Edward | Hi, Edward :)
      */
-    public String happilyGreet() {
-        return "Not implemented yet";
+    public String happilyGreet(String name) {
+        return "Hi, "+ name + " :)";
     }
 
     /*
@@ -64,7 +64,16 @@ public class Exercise extends ExerciseBase {
         10, 13 | [10,11,12,13]
         -1, 1  | [-1,0,1]
      */
+    public int[] constructNumberArray(int lower, int upper){
+        int count=lower;
+        int [] numberArray=new int[upper-lower+1];
+        while (count<=upper){
 
+            numberArray[count-lower]=count;
+            count++;
+        }
+        return numberArray;
+    }
 
 
 
@@ -81,7 +90,14 @@ public class Exercise extends ExerciseBase {
         error, 10   | ERROR!!!!!!!!!!
      */
 
+    public String shout(String thing, int num){
+        thing=thing.toUpperCase();
+        for (int i=0; i<num; i++) {
+            thing+="!";
+        }
+        return thing;
 
+    }
 
 
 }
